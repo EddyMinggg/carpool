d205bc2e07cb9cde009a784675bd8cb31a50911c
juan1fy <lkitleung2@gmail.com>
2025-09-08 18:29:18 +0800

    checkbox fix

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -67,12 +67,12 @@
 
                             <div class="flex mt-4">
                                 <div class="flex items-center h-5">
-                                    <input id="private-checkbox" type="checkbox"
+                                    <input id="private" type="checkbox"
                                         value=""
                                         class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded-sm focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
                                 </div>
                                 <div class="ms-2 text-sm">
-                                    <label for="private-checkbox"
+                                    <label for="private"
                                         class="font-medium text-gray-900 dark:text-gray-300">Private</label>
                                     <p id="private-checkbox-text"
                                         class="mt-1 text-xs font-normal text-gray-500 dark:text-gray-300">This will make your
