5216eb7f5a5c6df61066b0703a7fdf6c7b6aebd4
juan1fy <lkitleung2@gmail.com>
2025-09-25 22:07:26 +0800

    ajax session request fix

--- resources/views/map.blade.php
+++ resources/views/map.blade.php
@@ -3,7 +3,8 @@
     <div id="map"></div>
     <div class="fixed bottom-24 w-full">
         <div class="flex justify-center w-full">
-            <button id="select-button" class="w-1/2 bg-blue-600 hover:bg-blue-700 dark:bg-blue-700 dark:hover:bg-blue-800 text-white py-3 rounded-xl font-semibold text-md transition shadow-md hidden">
+            <button id="select-button"
+                class="w-1/2 bg-blue-600 hover:bg-blue-700 dark:bg-blue-700 dark:hover:bg-blue-800 text-white py-3 rounded-xl font-semibold text-md transition shadow-md hidden">
                 {{ __('Select') }}
             </button>
         </div>
@@ -17,9 +18,10 @@
         var _location = "";
 
         var map = L.map("map").setView([22.3, 114.1], 10);
-        var tiles = L.esri.Vector.vectorBasemapLayer(localStorage.getItem('dark-mode') === 'true' ? "arcgis/streets-night" : "arcgis/streets", {
-            token: _apiKey
-        }).addTo(map);
+        var tiles = L.esri.Vector.vectorBasemapLayer(localStorage.getItem('dark-mode') === 'true' ?
+            "arcgis/streets-night" : "arcgis/streets", {
+                token: _apiKey
+            }).addTo(map);
 
         var searchControl = L.esri.Geocoding.geosearch({
             useMapBounds: false,
@@ -42,17 +44,19 @@
             _location = data.text;
         });
 
-        $('#select-button').click(function () {
+        $('#select-button').click(function() {
             $.ajax({
                 url: "/set-session",
                 method: "POST",
                 data: {
                     _token: '{{ csrf_token() }}',
                     location: _location,
+                },
+                success: function(response) {
+                    $('#select-button').prop('disabled', true);
+                    window.location.replace("{{ route('dashboard') }}");
                 }
             });
-
-            window.location.replace("{{ route('dashboard') }}");
         });
 
     });
