7b0ffa3d80f7c7b7a7a3d7afc199960bec9c6705
Ming <q1240064774@gmail.com>
2025-10-25 16:04:30 +0800

    contact_update_2

--- resources/views/about/contact.blade.php
+++ resources/views/about/contact.blade.php
@@ -23,7 +23,7 @@
                         <span class="sm:hidden">WhatsApp</span>
                     </a>
 
-                    <a href="tel:+85298588879"
+                    <a href="tel:+85252414992"
                         class="flex items-center justify-center bg-blue-600 hover:bg-blue-700 text-white py-3 px-4 rounded-lg font-semibold transition">
                         <svg class="w-5 h-5 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z"></path>
@@ -56,7 +56,7 @@
                         </div>
                         <div class="ml-4">
                             <h3 class="text-lg font-semibold text-gray-900 dark:text-white">{{ __('Phone') }}</h3>
-                            <p class="text-gray-600 dark:text-gray-400">+852 9858 8879</p>
+                            <p class="text-gray-600 dark:text-gray-400">+852 5241 4992</p>
                             <p class="text-sm text-gray-500 dark:text-gray-500">{{ __('24/7 Customer Service') }}</p>
                         </div>
                     </div>
